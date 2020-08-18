d66a14cc3eca2bf7f073fd133163c65f9a06dcda
mahesdwivedi <mahesdwivedi@gmail.com>
2020-08-19 03:26:19 +0530

    Updates

---
+++ precache-manifest.0780f46b4bd1a78989b5de72c3f4bbdc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e185941a5842593ba32e3e6675840d30",
+    "url": "/whatschat/index.html"
+  },
+  {
+    "revision": "73115109b90989c687ea",
+    "url": "/whatschat/static/css/main.7db5069c.chunk.css"
+  },
+  {
+    "revision": "24d73f5ecdf70aa83eab",
+    "url": "/whatschat/static/js/2.dd23807e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/whatschat/static/js/2.dd23807e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73115109b90989c687ea",
+    "url": "/whatschat/static/js/main.30f48640.chunk.js"
+  },
+  {
+    "revision": "538af6eb94a177529f6a",
+    "url": "/whatschat/static/js/runtime-main.c2ab4a91.js"
+  },
+  {
+    "revision": "547b5bfbe22fdbc091892fcf55d53ac3",
+    "url": "/whatschat/static/media/CompanyImage.547b5bfb.png"
+  },
+  {
+    "revision": "00d05a7f9fb577224fa112d704081f21",
+    "url": "/whatschat/static/media/ill.00d05a7f.png"
+  },
+  {
+    "revision": "54638bf7b7d46cb628b66e20118f11d5",
+    "url": "/whatschat/static/media/logo.54638bf7.png"
+  },
+  {
+    "revision": "cd6c72ddbfe6a1fec6b5e2694ef8ba70",
+    "url": "/whatschat/static/media/secFive.cd6c72dd.PNG"
+  },
+  {
+    "revision": "ddb2e799256861c3a9824d25fcd6c12e",
+    "url": "/whatschat/static/media/secFour.ddb2e799.png"
+  },
+  {
+    "revision": "064cb0f65f3fa01970822bf32e0448d8",
+    "url": "/whatschat/static/media/secThree.064cb0f6.PNG"
+  }
+]);
